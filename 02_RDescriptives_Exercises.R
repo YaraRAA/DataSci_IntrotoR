7074f090cdd5d86fe838feb9eb72176624c4f55e
YaraRAA <33261257+YaraRAA@users.noreply.github.com>
2020-01-29 14:14:00 -0800

    Add files via upload

---
+++ 02_RDescriptives_Exercises.R
@@ -0,0 +1,29 @@
+
+#######################################
+#   Data Scietifique Workshops        #
+#   02 - Descriptive statistics in R  #
+#   EXERCISES                         #
+#   27 Jan  2020                      #
+#   Instructor: Yara Abu Awad         #
+#######################################
+
+
+## Base R comes with built-in data frames that you can use for practice. For the below exercises, we will be using the data frame iris.
+## While iris does not appear as an object in your workspace, you will be able to access it!
+
+## A- How many rows and columns does iris have? (hint: dim )
+## B- What type of data do the columns of iris contain? (hint: str )
+## C- What are the column names of iris? (hint: names )
+## D- HOW many missing values are there within each column of iris? (hint: summary for continuous columns and table with useNA for categorical columns)
+## E- What are the min, mean, median and max of the continuous column(s) in iris? (hint: summary )
+## F- What are the standard deviations the continuous column(s) in iris? (hint: sd )
+## F- GENERATE a frequency table of the column(s) in iris containing categorical data (hint: table)
+## G- CALCULATE the means of the first numerical column by groups in the first categorical column
+## H- USE a two sample t test to determine if the mean differs by category
+## I- RECODE the first continuous column into 0/1 by greater than/equal to or below the median
+## J- CONSTRUCT a 2x2 table of this new categorical column with the first categorical column in iris
+## K- ARE the factors independent? (hint: summary)
+
+
+
+#### WANT MORE? REPEAT A-F and I in the data frame mtcars
