6f94409693b5f0d889899ca37a14502ec7096063
YaraRAA <33261257+YaraRAA@users.noreply.github.com>
2020-01-29 14:13:37 -0800

    Add files via upload

---
+++ 01_IntrotoR_Exercises.R
@@ -0,0 +1,45 @@
+
+################################
+#   Data Scietifique Workshops #
+#   01 - Introduction to R     #
+#   EXERCISES                  #
+#   20 Jan  2020               #
+#   Instructor: Yara Abu Awad  #
+################################
+
+
+## A- CREATE a numeric vector named s which starts at 20 and ends at 100 in increments of 1 (hint: use seq function)
+
+## B- CREATE a numeric vector named x which is made of the numbers: 10,14,20 (hint: use c())
+
+
+
+## C- DETERMINE the length and data type of x and s (hint: use the length() & typeof() functions)
+
+
+
+## D- ADD the 80th element of s to the 2nd element of x (hint, use [] & the + sign)
+
+
+
+## E- ADD the 82nd element of s to the 2nd element of x - what error do you get and what does it mean?
+
+
+
+## F- CHANGE the second element of s to the number 7
+
+
+## G- DELETE the first element of s
+
+
+## H- ADD the letter 'a' to the end of the vector x
+
+
+## I- CHECK the data type of x now. What happened?
+
+#x is now a character vector
+
+
+## J- REMOVE s and x from your working directory (hint: rm() function or the broomstick)
+
+
